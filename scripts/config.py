a56cfdb79548dafc2063839f80dcd01a26f97640
edenwondwossen <edenwondwossen00@gmail.com>
2022-05-24 00:35:45 -0800

    dvc v1

--- scripts/config.py
+++ scripts/config.py
@@ -1,11 +1,9 @@
 from pathlib import Path
 
-
 class Config:
-    RANDOM_SEED = 42
-    ASSETS_PATH = Path("./data")
-    ORIGINAL_DATASET_FILE_PATH = ASSETS_PATH / "AdSmartABdata.csv"
-    DATASET_PATH = ASSETS_PATH / "data"
-    FEATURES_PATH = ASSETS_PATH / "features"
-    MODELS_PATH = ASSETS_PATH / "models"
-    METRICS_FILE_PATH = ASSETS_PATH / "metrics.json"
+  RANDOM_SEED = 90
+  DATASET_PATH = Path("../data")
+  REPO = "C:/Users/dell/OneDrive/Desktop/pharmaceutical-sales-prediction"
+  DATASET_FILE_PATH = DATASET_PATH / "train.csv"
+  FEATURES_PATH = DATASET_PATH / "features"
+  MODELS_PATH = Path("../models")
